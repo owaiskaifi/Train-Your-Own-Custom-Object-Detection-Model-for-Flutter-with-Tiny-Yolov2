f877c69ad6ada346def48c269dafa3e5778174c8
Muhammad Owais <43828642+owaiskaifi@users.noreply.github.com>
2022-07-09 23:21:40 +0500

    Update update_annotation.py

--- update_annotation.py
+++ update_annotation.py
@@ -1,10 +1,10 @@
 import glob
 import in_place
 
-path_to_folder = "D:\\Flutter\\YOLO_V2\\darkflow-master\\train\\annotation"
+path_to_folder = r"/content/Train-Your-Own-Custom-Object-Detection-Model-for-Flutter-with-Tiny-Yolov2/train/annotation/"
 
-old = "/content/drive/MyDrive/Colab Notebooks/blog.tensorflow/workspace/images/allImages/"
-new = "D:\\Flutter\\YOLO_V2\\darkflow\\train\\Images\\"
+old = "D:\\yolo\\labelthis\\"
+new = "/content/Train-Your-Own-Custom-Object-Detection-Model-for-Flutter-with-Tiny-Yolov2/train/Images/"
 
 for filepath in glob.glob( path_to_folder+'/*.xml'):
     with in_place.InPlace(filepath) as file:
